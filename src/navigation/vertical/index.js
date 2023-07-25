539b09928f4a1cca2af35713162343fdec82d0d3
duong-s-97 <duongdev97@gmail.com>
2023-07-25 14:45:26 +0700

    ui

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -121,16 +121,14 @@ const navigation = () => {
       role: ['Admin', 'Manager', 'EMPOYLEE'],
       disabled: true
     },
-<<<<<<< HEAD
-=======
+
     {
       icon: CreditCardOutline,
       title: 'Customer Service',
       path: '/customer-care',
       isShowMenu: true,
       role: ['Admin', 'Manager', 'EMPOYLEE']
-    }
->>>>>>> dev
+    },
 
     // {
     //   title: 'Documents',
@@ -196,7 +194,7 @@ const navigation = () => {
       isShowMenu: true,
       role: ['Admin', 'Manager', 'EMPOYLEE']
     }
-    
+
     // {
     //   title: 'Forgot Password',
     //   icon: Login,
